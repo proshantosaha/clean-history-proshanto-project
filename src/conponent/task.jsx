30c5e6d1b8d5035906305ad0782018f7e12ff416
proshantosaha <proshantoll71@gmail.com>
2023-05-21 15:39:22 +0600

    complet see ui and converted the json

---
+++ src/conponent/task.jsx
@@ -0,0 +1,66 @@
+const task = {
+    id:'uniquw_id_oo1',
+    subtitle: "Subtitle",
+
+    tags:[
+        {
+        id:'tag_001',
+        text:"its done",
+        icon:"T",
+       },
+       {
+        id:'tag_002',
+        text:"its cancelled",
+        icon:"x",
+       },
+       {
+        id:'tag_003',
+        text:"its in progress",
+        icon:"z",
+       },
+       {
+        id:'tag_004',
+        text:"just wrote this",
+        icon:"y",
+       }
+   ],
+
+   comments:[
+    {
+        id:"comment-id-001",
+        user:{
+            avatar:'xyz.com',
+            name:'Viraj',
+            id:'user-ud-001',
+        },
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vel? '  }
+   ],
+   text:[
+    {
+        id:'task_1',
+       title:'Foggy Nelson'   ,
+       text:'Here to clean the streets of Hells Kitchen'  ,
+       status:'done',
+   },
+   {
+    id:'task_2',
+    title:'Louis Ck'   ,
+    text:'its cancelled'  ,
+    status:'cancelled',
+},
+{   id:'task_3',
+    title:'Albert Elinstein'   ,
+    text:'its in progress'  ,
+    status:'progress',
+},
+{   id:'task_4',
+    title:'Albert Elinstein'   ,
+    text:'its in progress'  ,
+    status:'progress',
+}
+  ]
+}
+
+
+
+
